5dd9a024b23c3b4981b498be7bbd2d234bbec5ec
Brady Boun <bradyboun@gmail.com>
2025-05-05 16:14:00 -0700

    Added new plots

--- Synthesized_Dashboard/app.R
+++ Synthesized_Dashboard/app.R
@@ -19,6 +19,11 @@ library(sf)
 library(scales)
 library(rnaturalearth)
 library(rnaturalearthdata)
+library(supernova)
+library(stats)
+library(scales)
+library(readxl)
+library(dplyr)
 
 ### Seasonal Anomalies Code - START
 
@@ -101,20 +106,64 @@ function1 <-function(TempYears) {
   ggplotly(plotlyWTA_24, tooltip = "text") |>
     style(hoveron = "fill") 
 }
+
 ### Annual Anomalies END
 
+### Washington Emission Data Regression START
+
+WA <- read.csv("WAmonth.csv", skip = 3) |> 
+  separate(col = Date, into = c("Year", "Month"), sep = 4)|> 
+  group_by(Year) |> 
+  summarize (Temp = mean(Value)) |> 
+  ungroup()
+WA$Year <- as.numeric(WA$Year)
+
+emissions <- read_excel("US Energy CO2 Emissions.xlsx", skip = 4) |> 
+  filter(State == "Washington") |> 
+  select(`1970`:`2022`) |> 
+  pivot_longer(cols = `1970`:`2022`, names_to = "Year", values_to = "Emissions")
+emissions$Year <- as.numeric(emissions$Year)
+
+capita <- read_excel("energy_CO2_per_capita.xlsx", skip = 4) |> 
+  filter(State == "Washington") |> 
+  select(`1970`:`2022`) |> 
+  pivot_longer(cols = `1970`:`2022`, names_to = "Year", values_to = "Per_Capita")
+capita$Year <- as.numeric(capita$Year)
+
+WA_emit <- emissions |> 
+  left_join(capita, by = c("Year" = "Year")) |> 
+  left_join(WA, by = c("Year" = "Year"))
+
+AGGI <- read.csv("AGGI_Table.csv", skip = 2)
+
+AGGI <- AGGI[-seq(nrow(AGGI),nrow(AGGI)-3),] |> 
+  rename(Annual_PPM = Total,
+         Cumulative_PPM = Total.1,
+         Proportion_1990 = `X1990...1`,
+         Change_Percent_Previous = `change...`,
+         CFC = `CFC.`,
+         HFCs = `HFCs.`)
+AGGI$Year <- as.numeric(AGGI$Year)
+
+AGGI_emit <- WA_emit |> 
+  filter(Year >= 1979) |> 
+  left_join(AGGI, by = c("Year" = "Year"))
+
 # Define UI for application that draws a histogram
-ui <- dashboardPage(skin = "green",
-  dashboardHeader(title = "Climate Change Dashboard"),
-  dashboardSidebar(
+ui <- dashboardPage(skin = "blue",
+  dashboardHeader(title = "Climate Change Dashboard"), 
+sidebar <- dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabname = "dashboard", icon = icon("dashboard")))
+      menuItem("Anomalies", tabName = "anomalies_tab", icon = icon("dashboard")),
+      menuItem("Washington County", tabName = "wa_tab", icon = icon("dashboard")))
   ),
   dashboardBody(
+    tabItems(
+    tabItem(tabName = "anomalies_tab",
     fluidRow(
       box(
-        title = "Seasonal Anomalies", status = "primary", solidHeader = TRUE, 
-        collapsible = TRUE, width = 12,
+        title = "Seasonal Anomalies", solidHeader = TRUE, 
+        collapsible = TRUE, width = 12, background = "olive",
       sliderInput("yr", "Select Years for Season Map:",
                   min = min(temp_anomalies_v2$Year),
                   max = max(temp_anomalies_v2$Year),
@@ -125,14 +174,44 @@ ui <- dashboardPage(skin = "green",
                   selected = "Winter"),
         plotlyOutput("seasonPlot", height = 250),
       ),
-      box(title = "Annual Anomalies - World Map", status = "primary", solidHeader = TRUE,
-          collapsible = TRUE, width = 12,
+      box(title = "Annual Anomalies - World Map", solidHeader = TRUE,
+          collapsible = TRUE, width = 12, background = "olive",
           selectInput("TempYears", "Select Year for Global Map:",
                       choices = sort(unique(AnnTempAnomolies$Year)),
                       selected = 2024),
-          plotlyOutput("anomalies_mapPlot")))
+          plotlyOutput("anomalies_mapPlot"))
+    )
+    ),
+    tabItem(tabName = "wa_tab",
+            fluidRow(
+              box(
+                title = "Washington State's Annual Energy-Related Carbon Emissions", solidHeader = TRUE,
+                collapsible = TRUE, width = 6, background = "olive",
+                plotOutput("wa_emit_total")
+              ),
+              box(
+                title = "Washington State's Annual Energy-Related CO2 Emissions Per Capita", solidHeader = TRUE,
+                collapsible = TRUE, width = 6, background = "olive",
+                plotOutput("wa_emit_percapita")
+              ),
+              box(
+                title = "Washington State's Annual Average temperature", solidHeader = TRUE,
+                collapsible = TRUE, width = 6, background = "olive",
+                plotOutput("wa_avg_temp")
+              ),
+              box(
+                title = "Cumulative Parts Per Million (PPM)", solidHeader = TRUE,
+                collapsible = TRUE, width = 6, background = "olive",
+                plotOutput("wa_ppm")
+              )
+            )
+    )
+    )
   )
 )
+    
+
+    
 
   
 
@@ -148,6 +227,62 @@ server <- function(input, output) {
   output$anomalies_mapPlot <- renderPlotly({
     function1(TempYears = input$TempYears)
   })
+  
+  output$wa_emit_total <- renderPlot({
+    WA_emit |> 
+      ggplot(aes(x= Year, y = Emissions)) +
+      geom_point() +
+      geom_line() +
+      geom_smooth(method = "lm") +
+      theme_classic() +
+      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
+      labs(
+        y = "Million Metric Tons CO2"
+      )
+  })
+  
+  output$wa_emit_percapita <- renderPlot({
+    WA_emit |> 
+      ggplot(aes(x= Year, y = Per_Capita)) +
+      geom_point() +
+      geom_line() +
+      geom_smooth(method = "lm") +
+      theme_classic() +
+      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
+      labs(
+        title = "Washington State's Annual Energy-Related CO2 Emissions Per Capita",
+        y = "Metric Tons of CO2 Per Person"
+      )
+  })
+  
+  output$wa_avg_temp <- renderPlot({
+    WA |> 
+      ggplot(aes(x= Year, y = Temp)) +
+      geom_point() +
+      geom_line() +
+      geom_smooth(method = "lm") +
+      theme_classic() +
+      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
+      labs(
+        title = "Washington State's Annual Average temperature",
+        y = "Temperature (°F)"
+      )
+  })
+  
+  output$wa_ppm <- renderPlot({
+    AGGI_emit |> 
+      ggplot(aes(x= Year, y = Cumulative_PPM)) +
+      geom_point() +
+      geom_line() +
+      geom_smooth(method = "lm") +
+      theme_classic() +
+      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
+      labs(
+        title = "Cumulative PPM",
+        y = "Greenhouse Gas PPM"
+      )
+  })
+
 }
 
 # Run the application 
