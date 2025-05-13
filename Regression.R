781db16eb86e47011eec38659b1e939483716676
Mateo <mateocuadros451@gmail.com>
2025-05-12 20:45:24 -0700

    Added multiple regression models

--- Regression.R
+++ Regression.R
@@ -4,7 +4,7 @@ library(stats)
 library(scales)
 library(readxl)
 library(supernova)
-library(lm.beta)
+library(apaTables)
 
 WA <- read.csv("WAmonth.csv", skip = 3) |> 
   separate(col = Date, into = c("Year", "Month"), sep = 4)|> 
@@ -28,7 +28,7 @@ capita$Year <- as.numeric(capita$Year)
 AGGI <- read.csv("AGGI_Table.csv", skip = 2)
 
 AGGI <- AGGI[-seq(nrow(AGGI),nrow(AGGI)-3),] |> 
-  rename(Annual_PPM = Total,
+  rename(Total_Rad_Force = Total,
          Cumulative_PPM = Total.1,
          Proportion_1990 = `X1990...1`,
          Change_Percent_Previous = `change...`,
@@ -56,11 +56,10 @@ Combined <- WA_emit |>
   left_join(ocean_heat, by = c("Year" = "Year")) |> 
   left_join(sea_surface_temp, by = c("Year" = "Year"))
 
-Emissions_Oceans <- AGGI |> 
-  left_join(ocean_heat, by = c("Year" = "Year")) |> 
-  left_join(sea_surface_temp, by = c("Year" = "Year"))
-
-write.csv(Emissions_Oceans, "emissions_oceans.csv")
+#Emissions_Oceans <- AGGI |> 
+#  left_join(ocean_heat, by = c("Year" = "Year")) |> 
+#  left_join(sea_surface_temp, by = c("Year" = "Year"))
+#write.csv(Emissions_Oceans, "emissions_oceans.csv")
 
 
 #Significance Models
@@ -98,9 +97,9 @@ capitaModel <- lm(Temp ~ Per_Capita, data = WA_emit)
 capitaModel
 summary(capitaModel)
 
-AGGI_Annual_Model <- lm(Temp ~ Annual_PPM, data = Combined)
-AGGI_Annual_Model
-summary(AGGI_Annual_Model)
+AGGI_Rad_Model <- lm(Temp ~ Total_Rad_Force, data = Combined)
+AGGI_Rad_Model
+summary(AGGI_Rad_Model)
 
 AGGI_Cumulative_Model <- lm(Temp ~ Cumulative_PPM, data = Combined)
 AGGI_Cumulative_Model
@@ -112,8 +111,30 @@ summary(carbonModel)
 
 supernova(emitModel)
 supernova(capitaModel)
-supernova(AGGI_Annual_Model)
+supernova(AGGI_Rad_Model)
 supernova(AGGI_Cumulative_Model)
+
+#Original multiple regression model
+cor(Combined$Cumulative_PPM, Combined$Sea_Surface_Temp_Anomaly) #Pearson's r=.90
+WA_model <- lm(Temp ~ Cumulative_PPM + Sea_Surface_Temp_Anomaly, data = Combined)
+
+summary(WA_model)
+supernova(WA_model)
+confint(WA_model)
+
+
+#Improved multiple regression model (with justifications for new variables)
+#
+# Time was added as a predictor because there is a legit trend that is present
+# Rad_Force was added because it's a different variable than PPM (which I initially thought it was)
+# No ocean heat because sea surface temperature has a more direct affect on land temps
+#
+cor(Combined$Total_Rad_Force, Combined$Cumulative_PPM) #Pearson's r=.99
+
+Combo_model <- lm(Temp ~ Year + Sea_Surface_Temp_Anomaly + Total_Rad_Force + Cumulative_PPM, data = Combined)
+Combo_model
+summary(Combo_model)
+supernova(Combo_model)
 
 
 #Graphs
@@ -168,3 +189,16 @@ Combined |>
     title = "Cumulative PPM",
     y = "Greenhouse Gas PPM"
   )
+
+
+
+#Visual representation of multiple regression model - should we use it??
+Combined |> 
+  ggplot(aes(x = Year, y = Temp)) +
+  geom_line(linewidth = .5) +
+  geom_point(aes(alpha = Cumulative_PPM, color = Sea_Surface_Temp_Anomaly, size = Total_Rad_Force)) +
+  geom_point(aes(size = Total_Rad_Force), shape = 1, stroke = .8) +
+  scale_color_gradientn(colors = c("yellow", 'red', "red4")) +
+  scale_size(range = c(1,5)) +
+  scale_alpha(range = c(.4, 1))
+  
