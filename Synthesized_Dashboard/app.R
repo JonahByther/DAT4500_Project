af7736eb4934be20a4720f271fe685d46b957035
Brady Boun <bradyboun@gmail.com>
2025-05-15 19:17:01 -0700

    troubleshooted dashboard code and made county plot interactive

--- Synthesized_Dashboard/app.R
+++ Synthesized_Dashboard/app.R
@@ -110,59 +110,36 @@ function1 <-function(TempYears) {
 
 ### Annual Anomalies END
 
-### Washington Emission Data Regression START
+### WA County Significance
+WA_county <- read.csv("WA_county.csv")
 
-WA <- read.csv("WAmonth.csv", skip = 3) |> 
-  separate(col = Date, into = c("Year", "Month"), sep = 4)|> 
-  group_by(Year) |> 
-  summarize (Temp = mean(Value)) |> 
-  ungroup()
-WA$Year <- as.numeric(WA$Year)
-
-emissions <- read_excel("US Energy CO2 Emissions.xlsx", skip = 4) |> 
-  filter(State == "Washington") |> 
-  select(`1970`:`2022`) |> 
-  pivot_longer(cols = `1970`:`2022`, names_to = "Year", values_to = "Emissions")
-emissions$Year <- as.numeric(emissions$Year)
-
-capita <- read_excel("energy_CO2_per_capita.xlsx", skip = 4) |> 
-  filter(State == "Washington") |> 
-  select(`1970`:`2022`) |> 
-  pivot_longer(cols = `1970`:`2022`, names_to = "Year", values_to = "Per_Capita")
-capita$Year <- as.numeric(capita$Year)
-
-WA_emit <- emissions |> 
-  left_join(capita, by = c("Year" = "Year")) |> 
-  left_join(WA, by = c("Year" = "Year"))
-
-AGGI <- read.csv("AGGI_Table.csv", skip = 2)
-
-AGGI <- AGGI[-seq(nrow(AGGI),nrow(AGGI)-3),] |> 
-  rename(Annual_PPM = Total,
-         Cumulative_PPM = Total.1,
-         Proportion_1990 = `X1990...1`,
-         Change_Percent_Previous = `change...`,
-         CFC = `CFC.`,
-         HFCs = `HFCs.`)
-AGGI$Year <- as.numeric(AGGI$Year)
-
-AGGI_emit <- WA_emit |> 
-  filter(Year >= 1979) |> 
-  left_join(AGGI, by = c("Year" = "Year"))
-
-  # Regression Data Summaries
-
-emitSig <- lm(Emissions ~ Year, data = WA_emit)
-
-capitaSig <- lm(Per_Capita ~ Year, data = WA_emit)
-
-tempSig <-lm(Temp ~ Year, data = WA_emit)
-
-### WA State Regression END
+county_significance <- long_data |>
+  mutate(text = paste0("Category: ", Category,
+                       "\nMonth ", Month, 
+                       "\nSignificant Counties: ", Count)) |>
+  ggplot(aes(x = Month, y = Count, fill = Category)) +
+  geom_col(aes(text = text)) +
+  scale_fill_manual(
+    values = c(
+      "Positive_Slope_Counties" = "#FF0000",
+      "Not_Significant" = "#808080",
+      "Negative_Slope_Counties" = "#0000FF"
+    )
+  )+
+  labs(
+    x = "Month",
+    y = "Number of Counties",
+    fill = "Category"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+  
+  ggplotly(county_significance, tooltip = "text")
 
 ### Global Temp and Sea Regression
 
-annual_anomalies <- read_csv("annual_temperature_anomalies.csv")
 emissions_oceans <- read.csv("emissions_oceans.csv")
 
 annual_global_anomalies <- annual_anomalies |>
@@ -193,8 +170,6 @@ anom_ocean_heat <- joined_global_data |>
   ylab("Ocean Heat Content (10^22 Joules)") +
   xlab("Temperature Anomaly") +
   theme_minimal()
-
-ggplotly(anom_ocean_heat, tooltip = "text")
 
 
 # Define UI for application that draws a histogram
@@ -235,10 +210,9 @@ sidebar <- dashboardSidebar(
     tabItem(tabName = "wa_counties_tab",
             fluidRow(
               box(
-                title = "Washington County Map Average Temperature Comparison", solidHeader = TRUE,
+                title = "Washington Monthly Regression Summary Across Counties", solidHeader = TRUE,
                 collapsible = TRUE, width = 12, background = "olive",
-                selectInput("month", "Select a month:", choices = c("All Months", month.name), selected = "January"),
-                plotOutput("county_mapplot")
+                plotlyOutput("county_barplot")
               )
             )
     ),
@@ -255,7 +229,7 @@ sidebar <- dashboardSidebar(
     tabItem(tabName = "ocean_regression_tab",
             fluidRow(
               box(
-                title = "Ocean Regression",
+                solidHeader = TRUE,collapsible = TRUE, width = 6, background = "olive",
                 plotlyOutput("ocean_anom_graph")
               )
             )
@@ -282,83 +256,11 @@ server <- function(input, output) {
     function1(TempYears = input$TempYears)
   })
   
-  output$wa_emit_total <- renderPlot({
-    WA_emit |> 
-      ggplot(aes(x= Year, y = Emissions)) +
-      geom_point() +
-      geom_line() +
-      geom_smooth(method = "lm") +
-      theme_classic() +
-      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
-      labs(
-        title = "Washington State's Annual Energy-Related CO2 Emissions Per Capita",
-        y = "Million Metric Tons CO2"
-      )
-  })
-  
-  output$wa_emit_percapita <- renderPlot({
-    WA_emit |> 
-      ggplot(aes(x= Year, y = Per_Capita)) +
-      geom_point() +
-      geom_line() +
-      geom_smooth(method = "lm") +
-      theme_classic() +
-      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
-      labs(
-        title = "Washington State's Annual Energy-Related CO2 Emissions Per Capita",
-        y = "Metric Tons of CO2 Per Person"
-      )
-  })
-  
-  output$wa_avg_temp <- renderPlot({
-    WA |> 
-      ggplot(aes(x= Year, y = Temp)) +
-      geom_point() +
-      geom_line() +
-      geom_smooth(method = "lm") +
-      theme_classic() +
-      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
-      labs(
-        title = "Washington State's Annual Average temperature",
-        y = "Temperature (°F)"
-      )
-  })
-  
-  output$wa_ppm <- renderPlot({
-    AGGI_emit |> 
-      ggplot(aes(x= Year, y = Cumulative_PPM)) +
-      geom_point() +
-      geom_line() +
-      geom_smooth(method = "lm") +
-      theme_classic() +
-      scale_x_continuous(breaks=c(1970, 1980, 1990, 2000, 2010, 2020)) +
-      labs(
-        title = "Cumulative PPM",
-        y = "Greenhouse Gas PPM"
-      )
-  })
-  
-  output$county_mapplot <- renderPlot({
+
+  output$county_barplot <- renderPlotly({
     
-    ggplot(long_data, aes(x = Month, y = Count, fill = Category)) +
-      geom_col() +
-      scale_fill_manual(
-        values = c(
-          "Positive_Slope_Counties" = "#FF0000",
-          "Not_Significant" = "#808080",
-          "Negative_Slope_Counties" = "#0000FF"
-        )
-      )+
-      labs(
-        title = "Monthly Regression Summary Across Counties",
-        x = "Month",
-        y = "Number of Counties",
-        fill = "Category"
-      ) +
-      theme_minimal() +
-      theme(
-        axis.text.x = element_text(angle = 45, hjust = 1)
-      )
+    ggplotly(county_significance, tooltip = "text")
+    
   })
   
   output$ocean_anom_graph <- renderPlotly({
